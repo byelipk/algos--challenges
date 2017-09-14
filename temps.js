1ecb2049ec68e9dc526a5d8cc5c1d5f00e5f17b1
Patrick White <byelipk@kso2.local>
2017-09-14 14:09:50 -0400

    Added some notes

--- temps.js
+++ temps.js
@@ -1,18 +1,43 @@
 class TempTracker {
+  // What are the advantages and disadvantages of the approach
+  // we've taken here?
+  //
+  // This approach uses an ahead-of-time, greedy strategy for
+  // building the data structure.
+  //
+  // Specifically, we apply this strategy in the constructor and
+  // the insert method.
+  //
+  // By placing the bulk of the work there, we improve the
+  // efficiency of  our read operations to O(1)
   constructor() {
+
+    // We can lookup the min and max temperatures
+    // in constant time as long as we remember the
+    // BEST min and max values we've seen so far!
     this._max = null;
     this._min = null;
 
+    // We can caluclate the mean in constant time
+    // as long as we remember the number of records
+    // that have been inserted, and the current sum
+    // of temperatures.
     this._records = 0;
-    this._mean    = 0;
     this._sum     = 0;
+    this._mean    = 0;
 
     const MIN_TEMP = 0;
     const MAX_TEMP = 111;
 
+    // We can lookup the mode in constant time as
+    // long as we remember how many times we've
+    // recorded each temperature.
     this._mode = 0;
     this._occurences = [];
 
+    // Let's be greedy. Each index represents
+    // a temperature, and the value represents the number
+    // of times we've recorded the temperature.
     for (var i = MIN_TEMP; i < MAX_TEMP; i++) {
       this._occurences[i] = 0;
     }
