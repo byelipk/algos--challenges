f455a2f3ee439a6c8f0a69dbf1ce10ae137ab032
Patrick White <byelipk@kso2.local>
2017-09-14 13:54:38 -0400

    Make temps ways more efficient

--- temps.js
+++ temps.js
@@ -1,97 +1,60 @@
 class TempTracker {
   constructor() {
-    this._storage = [];
-    this._occurences  = { 0: 1 };
-    this._min     = 0;  // lowest recorded temp
-    this._max     = 0;  // highest recorded temp
-    this._mode    = 0;  // temp that occurres most frequently
-    this._mean    = 0;  // average temp
-  }
+    this._max = null;
+    this._min = null;
 
-  insert(temp) {
-    // make sure we're working with integers
-    const t = this._convertToInt(temp);
+    this._records = 0;
+    this._mean    = 0;
+    this._sum     = 0;
 
-    if (this._isValid(t)) {
+    const MIN_TEMP = 0;
+    const MAX_TEMP = 111;
 
-      this._storage.push(t); // add new temp
+    this._mode = 0;
+    this._occurences = [];
 
-      this._updateMin(t);
-      this._updateMax(t);
-      this._updateMode(t);
-      this._updateMean(t);
+    for (var i = MIN_TEMP; i < MAX_TEMP; i++) {
+      this._occurences[i] = 0;
     }
   }
 
-  _isValid(temp) {
-    if (temp >= 0 && temp <= 110) {
-      return true;
+  insert(t) {
+    // update max O(1)
+    if (this._max === null || t > this._max) {
+      this._max = t;
     }
-    return false;
-  }
 
-  _convertToInt(temp) {
-    return parseInt(temp, 10);
-  }
-
-  _updateMin(t) {
-    if (this._storage.length === 1) {
+    // update min O(1)
+    if (this._min === null || t < this._min) {
       this._min = t;
     }
-    else if (t < this._min) {
-      this._min = t;       // update min temp
+
+    // update mean O(1)
+    this._records += 1;
+    this._sum += t;
+    this._mean = this._sum / this._records;
+
+    // update mode O(1)
+    this._occurences[t] += 1;
+    if (this._occurences[t] > this._occurences[this._mode]) {
+      this._mode = t;
     }
   }
 
-  _updateMax(t) {
-    if (t > this._max) {
-      this._max = t;       // update max temp
-    }
-  }
-
-  // O(n)  O(1)
-  _updateMode(t) {
-    // update how many times we've recorded temperature t
-    this._occurences[t] =
-      (this._occurences[t] || 0) + 1;
-
-    // find the most frequently occurring temperature
-    let currentModeCount = this._occurences[this._mode];
-
-    for (let i = 0; i < this._storage.length; i++) {
-      const _temp = this._storage[i];
-      const tempRecordedCount = this._occurences[_temp];
-
-      if (tempRecordedCount > currentModeCount) {
-        this._mode = _temp;
-        currentModeCount = tempRecordedCount;
-      }
-    }
-  }
-
-  // O(n) O(1)
-  _updateMean(t) {
-    let sum = 0;
-    for (var i = 0; i < this._storage.length; i++) {
-      sum += this._storage[i];
-    }
-    this._mean = sum / this._storage.length;
-  }
-
-  getMax() {
-    return this._max;
-  }
-
-  getMin() {
+  getMin(t) {
     return this._min;
   }
 
-  getMode() {
-    return this._mode;
+  getMax(t) {
+    return this._max;
   }
 
   getMean() {
     return this._mean;
+  }
+
+  getMode() {
+    return this._mode;
   }
 }
 
