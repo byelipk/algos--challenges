ff8a417da8058c7f69c3e58929ef6444ed5bfdc0
Patrick White <byelipk@kso2.local>
2017-09-17 10:43:03 -0400

    More trie tests

--- __tests__/million-gazillion.test.js
+++ __tests__/million-gazillion.test.js
@@ -13,8 +13,14 @@ test('piano', () => {
 
   trie.add('piano');
   trie.add('pizza');
+  trie.add('purple');
+  trie.add('fun');
 
   expect(trie.find('piano')).toBe(true);
   expect(trie.find('pizza')).toBe(true);
+  expect(trie.find('purple')).toBe(true);
+  expect(trie.find('fun')).toBe(true);
   expect(trie.find('picasso')).toBe(false);
+  expect(trie.find('horatio')).toBe(false);
+  expect(trie.find('sanscript')).toBe(false);
 });
